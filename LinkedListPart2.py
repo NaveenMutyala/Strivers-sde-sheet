6b72d216dcc6ddbbaf8af90922cd90d17751bf45
Naveen kumar Mutyala <81841128+NaveenMutyala@users.noreply.github.com>
2024-02-01 23:27:23 +0530

    Update LinkedListPart2.py

--- LinkedListPart2.py
+++ LinkedListPart2.py
@@ -53,3 +53,56 @@ def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
             prevnode = temp
             temp = nextnode
         return head
+
+
+def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # stk =[]
+        if head is None or head.next is None:
+            return True
+        fast = head
+        slow = head
+        while fast.next != None and fast.next.next!= None:
+            fast = fast.next.next
+            # stk.append(slow.val)
+            slow = slow.next
+        slow.next = self.reverse(slow.next)
+        slow = slow.next
+        temp = head
+        while slow!= None:
+            if slow.val != temp.val:
+                return False
+            slow = slow.next
+            temp = temp.next
+              
+        return True
+    def reverse(self,ptr):
+        pre = None
+        next = None
+        while ptr != None:
+            next = ptr.next
+            ptr.next = pre
+
+            pre = ptr
+            ptr = next
+        return pre  
+
+
+
+def CycleNode(head):
+    # Write your code here
+    # pass
+    fast, slow = head, head
+    while fast != None and fast.next != None:
+    
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            fast = head
+            while fast != slow:
+                fast = fast.next
+                slow = slow.next
+            return fast
+    return None
+
+
+
